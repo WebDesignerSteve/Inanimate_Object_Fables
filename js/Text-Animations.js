440bbd630749fa6b3db721aeb8bf1e6c78c902d0
Steven <sttruong@s.sfusd.edu>
2019-04-23 17:51:48 -0700

    Quote Changes

---
+++ js/Text-Animations.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    /*! Fades in page on load */
+    $('#quote').css('display', 'none');
+    $('#quote').fadeIn(2500);
+
+    $('#author').css('display', 'none');
+    $('#author').fadeIn(2500);
+    
+});
